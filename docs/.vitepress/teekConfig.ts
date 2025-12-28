0aca316f8028a9c25fef973eb80d6a0462c34e42
panshuilong <987654psl,@>
2025-12-28 21:08:19 +0800

    first commit

---
+++ docs/.vitepress/teekConfig.ts
@@ -0,0 +1,36 @@
+import { defineTeekConfig } from "vitepress-theme-teek/config";
+import { version } from "vitepress-theme-teek/es/version";
+
+export const teekConfig = defineTeekConfig({
+  teekHome: true, // 是否开启博客首页
+  vpHome: true, // 是否隐藏 VP 首页
+  sidebarTrigger: true, // 是否开启侧边栏折叠功能
+  author: { name: "T.Y.B", link: "https://github.com/Kele-Bingtang" },
+  footerInfo: {
+    theme: {
+      name: `Theme By Teek@${version}`,
+    },
+    copyright: {
+      createYear: 2025,
+      suffix: "Teek",
+    },
+  },
+  codeBlock: {
+    copiedDone: (TkMessage) => TkMessage.success("复制成功！"),
+  },
+  articleShare: { enabled: true },
+  vitePlugins: {
+    sidebarOption: {
+      initItems: false,
+    },
+  },
+   banner: {
+     name: "", // Banner 标题，默认读取 vitepress 的 title 属性
+     bgStyle: "partImg", // Banner 背景风格：pure 为纯色背景，partImg 为局部图片背景，fullImg 为全屏图片背景
+     imgSrc: ["/blog/bg2.webp"], // Banner 图片链接。bgStyle 为 partImg 或 fullImg 时生效
+     descStyle: "default", // 描述信息风格：default 为纯文字渲染风格（如果 description 为数组，则取第一个），types 为文字打印风格，switch 为文字切换风格
+     description: [
+     
+    ], // 描述信息
+   }
+});
