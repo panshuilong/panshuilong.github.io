85bde06c06ac8c8f94b785503dd3e74f9bfbfa6e
unknown <panshuilong@xmmy.com>
2025-12-29 10:32:21 +0800

    add bg images

--- docs/.vitepress/teekConfig.ts
+++ docs/.vitepress/teekConfig.ts
@@ -27,7 +27,7 @@ export const teekConfig = defineTeekConfig({
    banner: {
      name: "", // Banner 标题，默认读取 vitepress 的 title 属性
      bgStyle: "partImg", // Banner 背景风格：pure 为纯色背景，partImg 为局部图片背景，fullImg 为全屏图片背景
-     imgSrc: ["/blog/bg2.webp"], // Banner 图片链接。bgStyle 为 partImg 或 fullImg 时生效
+     imgSrc: ["/blog/bg1.jpg"], // Banner 图片链接。bgStyle 为 partImg 或 fullImg 时生效
      descStyle: "default", // 描述信息风格：default 为纯文字渲染风格（如果 description 为数组，则取第一个），types 为文字打印风格，switch 为文字切换风格
      description: [
      
